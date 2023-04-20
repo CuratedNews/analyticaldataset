87f1aaa25767ee27f6d77b61ba9779c102fca94b
Matthew Benchimol <mdbench@curatednews.xyz>
2023-04-20 18:50:03 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# Analytical Dataset
+Our analytical dataset focuses on collating quality news-related data. We have made it open-source so users can see our data and use it as a reference. Our dataset is open-source and freely available.
+
+Check out our [interactive search page/demo](https://curatednews.github.io/analyticaldataset/) and/or [codebook](https://curatednews.github.io/analyticaldataset/CuratedNewsDatasetCodeBook.pdf)
+
+## Usage
+
+Our JSON dataset is available for testing purposes only [https://raw.githubusercontent.com/CuratedNews/analyticaldataset/main/CuratedNewsDataset.json](https://raw.githubusercontent.com/CuratedNews/analyticaldataset/main/CuratedNewsDataset.json)
+
+## Data format example
+
+### title, link, & date of news article
+```
+{
+ "title": "The three challenges keeping cars from being fully autonomous",
+ "link": "https://mittr-frontend-prod.herokuapp.com/s/613399/the-three-challenges-keeping-cars-from-being-fully-autonomous/",
+ "date": "2020-09-24 00:00:00 UTC",
+```
+
+### word count, [sentimentr score](https://github.com/trinker/sentimentr) (whole text), & [overall sentiment of article headline](https://curatednews.github.io/analyticaldataset#calculation-of-overall-sentiment-score)
+```
+ "titlewordcount": 9,
+ "titlesentiment": "0.333333333333333",
+ "titlesentimentoverall": "Positive",
+```
+
+### source, topic, & [leaning](https://curatednews.github.io/analyticaldataset/CuratedNewsDatasetCodeBook.pdf)
+```
+ "Source": "MIT",
+ "Topic": "Technology",
+ "Leaning": "Academic",
+``` 
+
+### other unique categorical variables (all unique variables must be operationalized/defined) 
+```
+ "President": "Trump"
+}
+```
+
+### complete example (minified)
+```
+{"title": "The three challenges keeping cars from being fully autonomous","link": "https://mittr-frontend-prod.herokuapp.com/s/613399/the-three-challenges-keeping-cars-from-being-fully-autonomous/","date": "2020-09-24 00:00:00 UTC","titlewordcount": 9,"titlesentiment": "0.333333333333333","titlesentimentoverall": "Positive","Source": "MIT","Topic": "Technology","Leaning": "Academic","President": "Trump"}
+```
+
+## Calculation of overall sentiment score
+  - Overall positive = sentimentr score > 0
+  - Overall negative = sentimentr score < 0
+  - Overall neutral = sentimentr score = 0
+
+## Other documentation/references
+- [sentimentr documentation](https://www.rdocumentation.org/packages/sentimentr/versions/2.9.0) for [R](https://www.r-project.org/about.html)
+- [sampling documentation](https://www.rdocumentation.org/packages/base/versions/3.6.2/topics/sample) for [R](https://www.r-project.org/about.html)
+- [Our codebook](https://curatednews.github.io/analyticaldataset/CuratedNewsDatasetCodeBook.pdf)
+
+## How it works?
+Check our [demo](https://curatednews.github.io/analyticaldataset/) for a hands-on with explanations
+We've made a headlines classifier with this dataset. Check out the [demo](https://curatednews.github.io/headlinesclassifier/)
+
+## Contribute
+Want to contribute? You can add unique categorical variables to the current data. Send us a pull request.
+
+## See our terms & conditions
+[Our terms & conditions](https://curatednews.xyz/curatedterms)
+
+[Our codebook](https://curatednews.github.io/analyticaldataset/CuratedNewsDatasetCodeBook.pdf)
+
+## Want to know more?
+Visit [https://curatednews.xyz](https://curatednews.xyz)
---
+++ static/js/analytics.js
@@ -0,0 +1,468 @@
+let searchcount = 0;
+let searchnocount = 0;
+const searchbar = document.getElementById("searchbar");
+const searchresults = document.getElementById('searchresults');
+const introtext = document.getElementById('introtext');
+
+searchbar.addEventListener("submit", function(e) {
+    e.preventDefault();
+    searchresults.innerHTML = "";
+    searchcount = 0;
+    searchnocount = 0;
+    console.log(searchcount);
+    let searchactual = document.getElementById("searchactual").value;
+    if (searchactual.length < 4){
+        introtext.innerHTML = "Query isn't long enough.";
+    }
+    if (searchactual.length < 4){
+        return;
+    }
+    data = randomize(data);
+    data.forEach(item => {
+        let emotionclass = "";
+        let leaningclass = "";
+        let importantfind = "";
+        const title = item["title"];
+        const link = item["link"];
+        const date = item["date"];
+        var newdate = new Date(date);
+        newdate = newdate.toDateString();
+        const titlesentimentoverall = item["titlesentimentoverall"];
+        const source = item["Source"];
+        const topic = item["Topic"];
+        const leaning = item["Leaning"];
+        if (title.toLowerCase().includes(searchactual.toLowerCase())){
+            searchcount++;
+            if (searchcount > 10){
+                return;
+            }
+            if (titlesentimentoverall.includes("Positive")){
+                emotionclass = "positivebutton";
+            } else if (titlesentimentoverall.includes("Negative")){
+                emotionclass = "negativebutton";
+            } else if (titlesentimentoverall.includes("Neutral")){
+                emotionclass = "neutralbutton";
+            }
+            if (leaning.includes("Conservative")){
+                leaningclass = "conservativeidmod";
+            } else if (leaning.includes("Liberal")){
+                leaningclass = "liberalidmod";
+            } else if (leaning.includes("Neutral")){
+                leaningclass = "neutralidmod";
+            } else if (leaning.includes("Academic")){
+                leaningclass = "unknownidmod";
+            } else if (leaning.includes("Unknown")){
+                leaningclass = "unknownidmod";
+            }
+            var outof = (searchnocount/total)*100;
+            searchresults.innerHTML += `<div class='newscardview'><div class="evaluation ${emotionclass}"><span id="evaluation${searchcount}">${titlesentimentoverall}</span></div><div class='post'><div class='post-head newscardtitle resizeallfeature'><span class="pubdate">${newdate}</span><h2 class='resizeallfeature textoverpicture'>${title}</h2><button id="modal-open${searchcount}" name="${source}" class="readmore_btn">${source}</button><a id="topic${searchcount}" class="regular_btn2 removeallfeature">${topic}</a><a id="leaning${searchcount}" class="regular_btn2 removeallfeature ${leaningclass}">${leaning}</a><a href='${link}' target='_blank' class='readmore_btn'>Read more</a>`;
+            if (outof < 2.5){
+                importantfind = "We were able to produce these results with less than 2.5% of our dataset. This search keyword is particularly important. Great find!";
+            } else if (outof > 15){
+                importantfind = "This search keyword isn't mentioned a lot in our dataset.";
+            } else if (outof > 90){
+                importantfind = "This search keyword isn't mentioned a lot in our dataset but its rarity leads us to believe you've stumbled onto something.";
+            }
+            introtext.innerHTML = `Here's a random sample from your search.<br><br><div id="factsandfiguresnotice" class="factsandfiguresnotice">We found these results after going through ${searchnocount} of ${total} data points, which is ~${outof.toFixed(2)}% of all available data in our dataset. ${importantfind}</div>`;
+        } else {
+            searchnocount++;
+        }
+        /*const date = item["date"];
+        const titlesentimentoverall = item["titlesentimentoverall"];
+        const newdate = new Date(date);
+        const month = newdate.getMonth();*/
+    });
+})
+
+let septembercount = 0;
+let octobercount = 0;
+let novembercount = 0;
+let decembercount = 0;
+let januarycount = 0;
+let februarycount = 0;
+let marchcount = 0;
+let aprilcount = 0;
+let maycount = 0;
+let junecount = 0;
+let julycount = 0;
+let augustcount = 0;
+
+let septemberpositivecount = 0;
+let septembernegativecount = 0;
+let septemberneutralcount = 0;
+
+let octoberpositivecount = 0;
+let octobernegativecount = 0;
+let octoberneutralcount = 0;
+
+let novemberpositivecount = 0;
+let novembernegativecount = 0;
+let novemberneutralcount = 0;
+
+let decemberpositivecount = 0;
+let decembernegativecount = 0;
+let decemberneutralcount = 0;
+
+let januarypositivecount = 0;
+let januarynegativecount = 0;
+let januaryneutralcount = 0;
+
+let februarypositivecount = 0;
+let februarynegativecount = 0;
+let februaryneutralcount = 0;
+
+let marchpositivecount = 0;
+let marchnegativecount = 0;
+let marchneutralcount = 0;
+
+let aprilpositivecount = 0;
+let aprilnegativecount = 0;
+let aprilneutralcount = 0;
+
+let maypositivecount = 0;
+let maynegativecount = 0;
+let mayneutralcount = 0;
+
+let junepositivecount = 0;
+let junenegativecount = 0;
+let juneneutralcount = 0;
+
+let julypositivecount = 0;
+let julynegativecount = 0;
+let julyneutralcount = 0;
+
+let augustpositivecount = 0;
+let augustnegativecount = 0;
+let augustneutralcount = 0;
+
+let overallpositivecount = 0;
+let overallnegativecount = 0;
+let overallneutral = 0;
+
+data.forEach(item => {
+    const date = item["date"];
+    const titlesentimentoverall = item["titlesentimentoverall"];
+    const newdate = new Date(date);
+    const month = newdate.getMonth();
+    calculateMonthlyTotals(month, titlesentimentoverall);
+});
+
+
+var total = overallpositivecount+overallnegativecount+overallneutral;
+var positive = (overallpositivecount/total)*100;
+document.getElementById("positive").innerText = `${positive.toFixed(0)}%`;
+var negative = (overallnegativecount/total)*100;
+document.getElementById("negative").innerText = `${negative.toFixed(0)}%`;
+var neutral = (overallneutral/total)*100;
+document.getElementById("neutral").innerText = `${neutral.toFixed(0)}%`;
+
+//Polar chart - chart1
+var chart1options = {
+    colors: ['#3c9d4e', '#e4bf58', '#4174c9'],
+    series: [overallpositivecount, overallnegativecount, overallneutral],
+    chart: {
+        type: 'polarArea',
+    },
+    stroke: {
+        colors: ['#fff']
+    },
+    fill: {
+        opacity: 1
+    },
+    labels: ['Positive','Negative','Neutral'
+    ],
+    legend: {
+        position: 'bottom',
+    },
+    responsive: [{
+        breakpoint: 480,
+        options: {
+            chart: {
+                width: 200
+            },
+            legend: {
+                position: 'bottom'
+            }
+        }
+    }]
+};
+
+var chart = new ApexCharts(document.querySelector("#chart1"), chart1options);
+chart.render();
+
+//Mixed chart - chart2
+var chart2options = {
+    colors: ['#000', '#e4bf58', '#3c9d4e', '#4174c9'],
+    series: [{
+        name: 'Total',
+        type: 'area',
+        data: [septembercount, octobercount, novembercount, decembercount, januarycount, februarycount, marchcount, aprilcount, maycount, junecount, julycount, augustcount]
+    }, {
+        name: 'Negative',
+        type: 'area',
+        data: [septembernegativecount, octobernegativecount, novembernegativecount, decembernegativecount, januarynegativecount, februarynegativecount, marchnegativecount, aprilnegativecount, maynegativecount, junenegativecount, julynegativecount, augustnegativecount]
+    } , {
+        name: 'Positive',
+        type: 'area',
+        data: [septemberpositivecount, octoberpositivecount, novemberpositivecount, decemberpositivecount, januarypositivecount, februarypositivecount, marchpositivecount, aprilpositivecount, maypositivecount, junepositivecount, julypositivecount, augustpositivecount]
+    } , {
+        name: 'Neutral',
+        type: 'area',
+        data: [septemberneutralcount, octoberneutralcount, novemberneutralcount, decemberneutralcount, januaryneutralcount, februaryneutralcount, marchneutralcount, aprilneutralcount, mayneutralcount, juneneutralcount, julyneutralcount, augustneutralcount]
+    }],
+    chart: {
+        height: 350,
+        type: 'line',
+        stacked: false,
+    },
+    title: {text: `Headlines volatility`,align: 'left',margin: 10,offsetX: 0,offsetY: 0,floating: false,style: {fontSize: '16px',fontWeight: 'bold',fontFamily: 'Poppins',color:  '#000'}},
+    stroke: {
+        width: [0, 2, 5],
+        curve: 'smooth'
+    },
+    plotOptions: {
+        bar: {
+            columnWidth: '50%'
+        }
+    },
+
+    fill: {
+        opacity: [1, 1, 1, 1],
+        gradient: {
+            inverseColors: false,
+            shade: 'light',
+            type: "vertical",
+            opacityFrom: 0.85,
+            opacityTo: 0.55,
+            stops: [0, 100, 100, 100]
+        }
+    },
+    labels: ['09/01/2020', '10/01/2020', '11/01/2020', '12/01/2020', '01/01/2021', '02/01/2021', '03/01/2021',
+        '04/01/2021', '05/01/2021', '06/01/2021', '07/01/2021', '08/01/2021'
+    ],
+    markers: {
+        size: 0
+    },
+    xaxis: {
+        title: {
+            text: ' ',
+        },
+        type: 'datetime'
+    },
+    yaxis: {
+        title: {
+            text: 'Articles',
+        },
+        min: 0
+    },
+    tooltip: {
+        shared: true,
+        intersect: false,
+        y: {
+            formatter: function (y) {
+                if (typeof y !== "undefined") {
+                    return y.toFixed(0) + " articles";
+                }
+                return y;
+
+            }
+        }
+    }
+};
+
+var chart = new ApexCharts(document.querySelector("#chart2"), chart2options);
+chart.render();
+
+window.onscroll = function() {scrollFunction()};
+let mybutton = document.getElementById("clearfilter");
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "inline-block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+let clearall = document.getElementById("clearall");
+clearall.addEventListener("click", function(evnt){
+    searchresults.innerHTML = "";
+    document.getElementById('introtext').innerText = "Make another search.";
+    if (document.getElementById('factsandfiguresnotice') !== null){
+        document.getElementById('factsandfiguresnotice').style.display = "none";
+    }
+    setTimeout(scrollTop, 200);
+});
+
+let clearall2 = document.getElementById("clearall2");
+clearall2.addEventListener("click", function(evnt){
+    searchresults.innerHTML = "";
+    document.getElementById('introtext').innerText = "Make another search.";
+    if (document.getElementById('factsandfiguresnotice') !== null){
+        document.getElementById('factsandfiguresnotice').style.display = "none";
+    }
+    setTimeout(scrollTop, 200);
+});
+
+function scrollTop(){
+    window.location.replace("#top");
+}
+
+function calculateMonthlyTotals(month, titlesentimentoverall){
+    if (month === 8){
+        septembercount++;
+        if (titlesentimentoverall.includes("Positive")){
+            overallpositivecount++;
+            septemberpositivecount++;
+        } else if (titlesentimentoverall.includes("Negative")){
+            overallnegativecount++;
+            septembernegativecount++;
+        } else if (titlesentimentoverall.includes("Neutral")){
+            overallneutral++;
+            septemberneutralcount++;
+        }
+    } else if (month === 9){
+        octobercount++;
+        if (titlesentimentoverall.includes("Positive")){
+            overallpositivecount++;
+            octoberpositivecount++;
+        } else if (titlesentimentoverall.includes("Negative")){
+            overallnegativecount++;
+            octobernegativecount++;
+        } else if (titlesentimentoverall.includes("Neutral")){
+            overallneutral++;
+            octoberneutralcount++;
+        }
+    } else if (month === 10){
+        novembercount++;
+        if (titlesentimentoverall.includes("Positive")){
+            overallpositivecount++;
+            novemberpositivecount++;
+        } else if (titlesentimentoverall.includes("Negative")){
+            overallnegativecount++;
+            novembernegativecount++;
+        } else if (titlesentimentoverall.includes("Neutral")){
+            overallneutral++;
+            novemberneutralcount++;
+        }
+    } else if (month === 11){
+        decembercount++;
+        if (titlesentimentoverall.includes("Positive")){
+            overallpositivecount++;
+            decemberpositivecount++;
+        } else if (titlesentimentoverall.includes("Negative")){
+            overallnegativecount++;
+            decembernegativecount++;
+        } else if (titlesentimentoverall.includes("Neutral")){
+            overallneutral++;
+            decemberneutralcount++;
+        }
+    } else if (month === 0){
+        januarycount++;
+        if (titlesentimentoverall.includes("Positive")){
+            overallpositivecount++;
+            januarypositivecount++;
+        } else if (titlesentimentoverall.includes("Negative")){
+            overallnegativecount++;
+            januarynegativecount++;
+        } else if (titlesentimentoverall.includes("Neutral")){
+            overallneutral++;
+            januaryneutralcount++;
+        }
+    } else if (month === 1){
+        februarycount++;
+        if (titlesentimentoverall.includes("Positive")){
+            overallpositivecount++;
+            februarypositivecount++;
+        } else if (titlesentimentoverall.includes("Negative")){
+            overallnegativecount++;
+            februarynegativecount++;
+        } else if (titlesentimentoverall.includes("Neutral")){
+            overallneutral++;
+            februaryneutralcount++;
+        }
+    } else if (month === 2){
+        marchcount++;
+        if (titlesentimentoverall.includes("Positive")){
+            overallpositivecount++;
+            marchpositivecount++;
+        } else if (titlesentimentoverall.includes("Negative")){
+            overallnegativecount++;
+            marchnegativecount++;
+        } else if (titlesentimentoverall.includes("Neutral")){
+            overallneutral++;
+            marchneutralcount++;
+        }
+    } else if (month === 3){
+        aprilcount++;
+        if (titlesentimentoverall.includes("Positive")){
+            overallpositivecount++;
+            aprilpositivecount++;
+        } else if (titlesentimentoverall.includes("Negative")){
+            overallnegativecount++;
+            aprilnegativecount++;
+        } else if (titlesentimentoverall.includes("Neutral")){
+            overallneutral++;
+            aprilneutralcount++;
+        }
+    } else if (month === 4){
+        maycount++;
+        if (titlesentimentoverall.includes("Positive")){
+            overallpositivecount++;
+            maypositivecount++;
+        } else if (titlesentimentoverall.includes("Negative")){
+            overallnegativecount++;
+            maynegativecount++;
+        } else if (titlesentimentoverall.includes("Neutral")){
+            overallneutral++;
+            mayneutralcount++;
+        }
+    } else if (month === 5){
+        junecount++;
+        if (titlesentimentoverall.includes("Positive")){
+            overallpositivecount++;
+            junepositivecount++;
+        } else if (titlesentimentoverall.includes("Negative")){
+            overallnegativecount++;
+            junenegativecount++;
+        } else if (titlesentimentoverall.includes("Neutral")){
+            overallneutral++;
+            juneneutralcount++;
+        }
+    } else if (month === 6){
+        julycount++;
+        if (titlesentimentoverall.includes("Positive")){
+            overallpositivecount++;
+            julypositivecount++;
+        } else if (titlesentimentoverall.includes("Negative")){
+            overallnegativecount++;
+            julynegativecount++;
+        } else if (titlesentimentoverall.includes("Neutral")){
+            overallneutral++;
+            julyneutralcount++;
+        }
+    } else if (month === 7){
+        augustcount++;
+        if (titlesentimentoverall.includes("Positive")){
+            overallpositivecount++;
+            augustpositivecount++;
+        } else if (titlesentimentoverall.includes("Negative")){
+            overallnegativecount++;
+            augustnegativecount++;
+        } else if (titlesentimentoverall.includes("Neutral")){
+            overallneutral++;
+            augustneutralcount++;
+        }
+    }
+}
+
+function randomize(values) {
+    let index = values.length,
+        randomIndex;
+    while (index != 0) {
+        randomIndex = Math.floor(Math.random() * index);
+        index--;
+        [values[index], values[randomIndex]] = [values[randomIndex], values[index]];
+    }
+    return values;
+}
